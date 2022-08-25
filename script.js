3a84222b5d5d3e7e1655a485421f0031b7d59704
yomnatareksaber <yomnaaaat@gmail.com>
2022-08-25 11:54:30 +0300

    class work 9 :angry:

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // أنشئ المصفوفة تحت هذا الكومنت
 
+let orders = [] ;
+
 
 function addOrder() {
   let name = document.getElementById("name").value;
@@ -8,16 +10,21 @@ function addOrder() {
   let order = document.getElementById("order").value;
   
   // أنشئ كائن newOrder
-  
+  let newOrder = { name , id, phone,order}
   // أدرج الكائن إلى المصفوفة
-  
+  orders.push(newOrder)
   // طبق الخطوة 7 تحت هذا الكومنت
-  
+  load()
 }
 
 function load() {
   let container = document.getElementById("container");
-  
+  container.innerHTML = "";
   // طبّق الخطوة 6 تحت هذا الكومنت
+  orders.forEach(item => {
+    container.innerHTML+=
+    `<div><h1>${item.name} - ${item.id}</h1><h3>${item.order}</h3></div>`
+
+  });
 
 }
